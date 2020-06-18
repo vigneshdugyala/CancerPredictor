7341194a662f6301c474a697c679e42b1b46618a
vigneshdugyala <vigneshdugyala980@gmail.com>
2020-06-18 22:55:41 +0530

    third

--- detect/urls.py
+++ detect/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('',views.home,name="home"),path('start',views.start,name="start"),path('about',views.about,name="about"),
+    path('home',views.home,name="home"),path('start',views.start,name="start"),path('about',views.about,name="about"),
     path('home',views.home,name="start")
 ]
